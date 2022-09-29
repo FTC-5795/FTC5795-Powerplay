b94e251548d8e9b5d31a763df34a8eb9a31d5ea2
OmOza5785 <ozaop@guilford.edu>
2022-09-29 13:03:44 -0400

    Committing Turn Measurements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/BasicTeleOp.java
@@ -1,11 +1,15 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.util.ElapsedTime;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
+import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 @TeleOp(name="BasicTeleOp", group = "a")
 public class BasicTeleOp extends LinearOpMode {
@@ -15,6 +19,24 @@ public class BasicTeleOp extends LinearOpMode {
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        BNO055IMU imu;
+        BNO055IMU.Parameters gyro = new BNO055IMU.Parameters();
+        gyro.angleUnit = BNO055IMU.AngleUnit.DEGREES;
+        gyro.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        //gyro.calibrationDataFile = "AdafruitIMUCalibration.json"; // see the calibration sample op mode
+        gyro.mode = BNO055IMU.SensorMode.IMU;
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+
+        imu.initialize(gyro);
+
+        Orientation angles;
+
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+
+        telemetry.addData("Mode", "calibrating...");
+        telemetry.update();
+
         fL = hardwareMap.get(DcMotorEx.class, "leftFront");
         fR = hardwareMap.get(DcMotorEx.class, "rightFront");
         bL = hardwareMap.get(DcMotorEx.class, "leftRear");
@@ -28,11 +50,31 @@ public class BasicTeleOp extends LinearOpMode {
             double y = -gamepad1.left_stick_y;
             double x = gamepad1.left_stick_x * 1.1;
             double rx = gamepad1.right_stick_x;
-
+            telemetry.addData("turn", rx);
+            /*
             flPower = y + x - rx;
             frPower = y - x + rx;
             blPower = y - x - rx;
             brPower = y + x + rx;
+            */
+
+            if(rx < .5) {
+                flPower = y + x - rx;
+                frPower = y - x + rx;
+                blPower = y - x - rx;
+                brPower = y + x + rx;
+
+            }
+
+            if(rx > .5) {
+                flPower = y + x - 1;
+                frPower = y - x + 1;
+                blPower = y - x - 1;
+                brPower = y + x + 1;
+                telemetry.update();
+                telemetry.addData("Angle", angles.secondAngle);
+                telemetry.update();
+            }
 
             double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
             flPower /= denominator;
@@ -47,9 +89,9 @@ public class BasicTeleOp extends LinearOpMode {
 
             DcMotorEx intake = hardwareMap.get(DcMotorEx.class, "intakeMotor");
 
-            double fly = gamepad1.right_trigger;
+            double flyPower = -1 * gamepad1.right_trigger;
 
-            intake.setPower(fly);
+            intake.setPower(flyPower);
         }
 
 
