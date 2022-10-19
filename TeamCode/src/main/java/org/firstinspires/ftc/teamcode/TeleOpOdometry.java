e0aced799123a608297acd09d354c4057e77b77c
mdanley1234 <112212314+mdanley1234@users.noreply.github.com>
2022-10-18 20:30:35 -0400

    TeleOpOdometry Update -Revised code for 3 wheel odometry -Requires following configuration: >width & y_Offset constants need to calculated >direction of odo pods need to be implemented >encoders need to be ported to correct motor location on hub

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpOdometry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpOdometry.java
@@ -3,6 +3,7 @@ package org.firstinspires.ftc.teamcode;
 import com.acmerobotics.roadrunner.geometry.Pose2d;
 import com.qualcomm.hardware.bosch.BNO055IMU;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
@@ -10,90 +11,78 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 /*
-Currently implemented for two dead wheel odometry
+Currently implemented for three dead wheel odometry
 
-Notes: -Start position is init position as (0,0) and 90 degrees (Unit Circle) as forward
--16 bit overflow error when using getVelocity function on Rev Encoders means velocity as to be calculated through getCurrentPosition function
+Notes: -Start position set as (0,0) and 0 degrees (Unit Circle)
  */
 
 @TeleOp
 
 public class TeleOpOdometry extends LinearOpMode {
 
-    //Odometry pod electronic variables
-    public static double ticksPerRevolution = 8192;
-    public static double wheelRadius = 0.5; //inches
-    public static double gearRatio = 1;
-
-    //Odometry pod placement variables
-    public static double parallelX = 0; //inches
-    public static double parallelY = 0; //inches
-    public static double perpendicularX = 0; //inches
-    public static double perpendicularY = 0; //inches
-
-    //Motor & Odometry declarations
     private DcMotorEx fL, fR, bL, bR;
     private double fLPower, fRPower, bLPower, bRPower;
-    private DcMotorEx perpendicularEncoder, parallelEncoder;
-    private double perpendicularDistance, parallelDistance;
-    private double oldPerpendicularDistance, oldParallelDistance;
-    private Pose2d position = new Pose2d(0, 0, 90); //Start position
+    private DcMotorEx encoderParaL, encoderParaR, encoderPerp;
 
-    //Other declarations
-    private ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+    //Geometric constants of Odometry
+    private static double width = ; //distance between encoderParaL and encoderParaR in inches
+    private static double y_Offset = ; //distance between midpoint of encoderParaL & encoderParaR AND encoderPerp in inches
+    private static double wheelDiameter = 1; //Odometry wheel diameter in inches
+    private static int encoderTicks = 8192; //Ticks per revolution of REV Bore Encoder
+    private static double inchesPerTick = (wheelDiameter * Math.PI)/(encoderTicks);
+
+    //Positioning constants (counted in ticks not inches)
+    private int currentParaL = 0;
+    private int currentParaR = 0;
+    private int currentPerp = 0;
+
+    private int previousParaL = 0;
+    private int previousParaR = 0;
+    private int previousPerp = 0;
+
+    //Starting and current positions/heading of bot (counted in inches and degrees)
+    private double xPosition = 0;
+    private double yPosition = 0;
+    private double heading = 0;
 
     @Override
-    public void runOpMode() throws InterruptedException{
+    public void runOpMode() throws InterruptedException {
 
-        telemetry.addData("System:", "calibrating...");
+        telemetry.addData("Status", "Calibrating...");
         telemetry.update();
 
-        //imu Mapping
-        BNO055IMU imu;
-        BNO055IMU.Parameters gyro = new BNO055IMU.Parameters();
-        gyro.angleUnit = BNO055IMU.AngleUnit.DEGREES;
-        gyro.accelUnit = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
-        //gyro.calibrationDataFile = "AdafruitIMUCalibration.json"; //see the calibration sample op mode
-        gyro.mode = BNO055IMU.SensorMode.IMU;
-        imu = hardwareMap.get(BNO055IMU.class, "imu");
-
-        imu.initialize(gyro);
-
-        //Motor Mapping
+        //Motor mapping and calibration
         fL = hardwareMap.get(DcMotorEx.class, "leftFront");
+        fL.setDirection(DcMotorSimple.Direction.FORWARD);
+        fL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         fR = hardwareMap.get(DcMotorEx.class, "rightFront");
-        bL = hardwareMap.get(DcMotorEx.class, "leftRear");
-        bR = hardwareMap.get(DcMotorEx.class, "rightRear");
-
-        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fL.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        fR.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        bL.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        bL.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
-        bR.setMode(DcMotorEx.RunMode.STOP_AND_RESET_ENCODER);
-        bR.setMode(DcMotorEx.RunMode.RUN_WITHOUT_ENCODER);
-
         fR.setDirection(DcMotorSimple.Direction.REVERSE);
+        fR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        bL = hardwareMap.get(DcMotorEx.class, "leftRear");
+        bL.setDirection(DcMotorSimple.Direction.FORWARD);
+        bL.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        bR = hardwareMap.get(DcMotorEx.class, "rightRear");
         bR.setDirection(DcMotorSimple.Direction.REVERSE);
+        bR.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
-        //Link Odometry Pods to Motor Ports (Motors and Pods not related)
-        perpendicularEncoder = fL;
-        parallelEncoder = bL;
+        //Shadowing motors with encoders (for porting purposes)
+        encoderParaL = fL;
+        encoderParaR = fR;
+        encoderPerp = bL;
 
-        telemetry.addData("System:", "calibration complete");
+        resetEncoders();
+
+        telemetry.addData("Status", "Calibration Complete");
         telemetry.update();
 
         waitForStart();
 
         while (opModeIsActive()) {
-
-            //Tele-Op Code
             double y = -gamepad1.left_stick_y;
-            double x = gamepad1.left_stick_x * 1.1;
+            double x = gamepad1.left_stick_x;
             double rx = gamepad1.right_stick_x;
 
             fLPower = y + x - rx;
@@ -101,27 +90,66 @@ public class TeleOpOdometry extends LinearOpMode {
             bLPower = y - x - rx;
             bRPower = y + x + rx;
 
+            double denominator = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
+
+            fLPower /= denominator;
+            fRPower /= denominator;
+            bLPower /= denominator;
+            bRPower /= denominator;
+
             fL.setPower(fLPower);
             fR.setPower(fRPower);
             bL.setPower(bLPower);
             bR.setPower(bRPower);
 
-            //Odometry Code
-            oldParallelDistance = parallelDistance;
-            oldPerpendicularDistance = perpendicularDistance;
+            odometry();
 
-            parallelDistance = parallelEncoder.getCurrentPosition();
-            perpendicularDistance = perpendicularEncoder.getCurrentPosition();
-
-            double parallelChange = parallelDistance - oldParallelDistance;
-            double perpendicularChange = perpendicularDistance - oldPerpendicularDistance;
-            if (imu.getAngularOrientation().firstAngle>=0) {
-//                position.component3() = imu.getAngularOrientation().firstAngle + 90; //facing forward
-            } else if (imu.getAngularOrientation().firstAngle<0) {
-//                position.component3() = imu.getAngularOrientation().firstAngle + 450; //facing forward and positive
-            }
-
+            telemetry.addData("xPosition", xPosition);
+            telemetry.addData("yPosition", yPosition);
+            telemetry.addData("heading", heading);
+            telemetry.update();
         }
     }
 
+    public void resetEncoders() {
+        fL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        bL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        bL.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        fR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        fR.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+    }
+
+    //Odometry code
+    public void odometry() {
+        previousParaL = currentParaL;
+        previousParaR = currentParaR;
+        previousPerp = currentPerp;
+
+        //Negate following to reverse encoder direction
+        currentParaL = encoderParaL.getCurrentPosition();
+        currentParaR = -encoderParaR.getCurrentPosition();
+        currentPerp = encoderPerp.getCurrentPosition();
+
+        //change in encoders in ticks
+        int deltaParaL = currentParaL - previousParaL; //change in ParaL
+        int deltaParaR = currentParaR - previousParaR; //change in ParaR
+        int deltaPerp = currentPerp - previousPerp; //change in Perp
+
+        //change in position (relative to bot) in inches
+        double deltaX = inchesPerTick * (deltaParaL + deltaParaR) / 2;
+        double deltaY = inchesPerTick * (deltaPerp - (deltaParaR - deltaParaL) * (y_Offset / width));
+        double deltaTheta = inchesPerTick * (deltaParaR - deltaParaL) / width;
+
+        //position update (relative to field) in inches and degrees
+        double theta = heading + Math.toDegrees(deltaTheta / 2); //theta used for trig calculations
+        xPosition += deltaX * Math.cos(Math.toRadians(theta)) - deltaY * Math.sin(Math.toRadians(theta));
+        yPosition += deltaX * Math.sin(Math.toRadians(theta)) + deltaY * Math.cos(Math.toRadians(theta));
+        heading += deltaTheta;
+    }
 }
