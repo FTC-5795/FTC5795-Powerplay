584ef2feb4a03b6d9ce11c6d48220770debd1911
SophieR25 <srocco@guilford.edu>
2023-01-15 16:53:16 -0500

    More updates

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCode/RoadRunnerTrajectory.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mainCode/RoadRunnerTrajectory.java
@@ -8,34 +8,47 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.mainCode.functionClasses.gripServoController;
+import org.firstinspires.ftc.teamcode.mainCode.functionClasses.vSlideMotorController;
 import org.firstinspires.ftc.teamcode.otherCode.drive.SampleMecanumDrive;
 import org.firstinspires.ftc.teamcode.otherCode.trajectorysequence.TrajectorySequence;
 
 /*
  * This is an example of a more complex path to really test the tuning.
  */
+
+//   SlideLevel.autoVSlide(0-12);
+//        Grab.autoGrip(TRUE/FALSE);
+
 @Autonomous(group = "drive")
 public class RoadRunnerTrajectory extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
+        vSlideMotorController SlideLevel = new vSlideMotorController(hardwareMap);
+        gripServoController Grab = new gripServoController(hardwareMap);
 
         // Loading up trajectories before start
 
         drive.setPoseEstimate(new Pose2d(-35, 61.5, Math.toRadians(270)));
 
         TrajectorySequence Right1 = drive.trajectorySequenceBuilder(new Pose2d(-35, 61.5, Math.toRadians(270)))
-                .splineToConstantHeading(new Vector2d(-24,12), Math.toRadians(0))
-                .lineToSplineHeading(new Pose2d(-20, 12, Math.toRadians(270)))
+                .addDisplacementMarker(() -> {
+                    telemetry.addData("A", "B");
+                    telemetry.update();
+                })
+                .splineToConstantHeading(new Vector2d(-24,13.5), Math.toRadians(0))
+                .lineToSplineHeading(new Pose2d(-20, 13.5, Math.toRadians(270)))
                 .waitSeconds(.5)
-                .lineToSplineHeading(new Pose2d(-25,12, Math.toRadians(270)))
-                .lineToSplineHeading(new Pose2d(-40,12, Math.toRadians(180)))
-                .splineToConstantHeading(new Vector2d(-55, 12), Math.toRadians(180))
+                .lineToSplineHeading(new Pose2d(-25,13.5, Math.toRadians(270)))
+                .lineToSplineHeading(new Pose2d(-50,13.5, Math.toRadians(180)))
+                .splineToConstantHeading(new Vector2d(-55, 10), Math.toRadians(180))
                 .waitSeconds(.5)
-                .back(12)
-                .lineToSplineHeading(new Pose2d(-20,12, Math.toRadians(270)))
+                .splineToConstantHeading(new Vector2d(-50, 13.5), Math.toRadians(180))
+                .back(7)
+                .lineToSplineHeading(new Pose2d(-20,13.5, Math.toRadians(270)))
                 .waitSeconds(.5)
-                .lineToSplineHeading(new Pose2d(-10,12, Math.toRadians(270)))
+                .lineToSplineHeading(new Pose2d(-10,13.5, Math.toRadians(270)))
                 .back(24)
                 .build();
 
@@ -113,6 +126,8 @@ public class RoadRunnerTrajectory extends LinearOpMode {
 //                .lineToSplineHeading(new Pose2d(58,12, Math.toRadians(270)))
 //                .back(24)
 //                .build();
+        Grab.autoGrip(true);
+
 
         //after start has been pressed
         waitForStart();
@@ -123,17 +138,60 @@ public class RoadRunnerTrajectory extends LinearOpMode {
         sleep(2000);
         drive.followTrajectorySequence(
                         drive.trajectorySequenceBuilder(Right1.end())
-                                .splineToConstantHeading(new Vector2d(-24,12), Math.toRadians(0))
-                                .lineToSplineHeading(new Pose2d(-20, 12, Math.toRadians(270)))
+
+                                .addDisplacementMarker(() -> {
+                                    telemetry.addData("A", "B");
+                                    telemetry.update();
+                                })
+
+//                                .UNSTABLE_addTemporalMarkerOffset(0.5, () ->
+//                                {SlideLevel.autoVSlide(2);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(1, () ->
+//                                {SlideLevel.autoVSlide(12);})
+////
+//                                .UNSTABLE_addTemporalMarkerOffset(3.5, () ->
+//                                {Grab.autoGrip(false);})
+
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(4, () ->
+//                                {SlideLevel.autoVSlide(10);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(5, () ->
+//                                {SlideLevel.autoVSlide(9);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(5.5, () ->
+//                                {Grab.autoGrip(true);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(5.75, () ->
+//                                {SlideLevel.autoVSlide(10);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(6, () ->
+//                                {SlideLevel.autoVSlide(12);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(7, () ->
+//                                {Grab.autoGrip(false);})
+//
+//                                .UNSTABLE_addTemporalMarkerOffset(7.5, () ->
+//                                {SlideLevel.autoVSlide(0);})
+
+//                                .addDisplacementMarker(() -> {
+//                                    telemetry.addData("I works", "true");
+//                                    telemetry.update();
+//                                })
+
+                                .splineToConstantHeading(new Vector2d(-24,13.5), Math.toRadians(0))
+                                .lineToSplineHeading(new Pose2d(-20, 13.5, Math.toRadians(270)))
+                                .waitSeconds(3)
+                                .lineToSplineHeading(new Pose2d(-25,13.5, Math.toRadians(270)))
+                                .lineToSplineHeading(new Pose2d(-53,13.5, Math.toRadians(180)))
+                                .lineToSplineHeading(new Pose2d(-55, 12, Math.toRadians(180)))
                                 .waitSeconds(.5)
-                                .lineToSplineHeading(new Pose2d(-25,12, Math.toRadians(270)))
-                                .lineToSplineHeading(new Pose2d(-40,12, Math.toRadians(180)))
-                                .splineToConstantHeading(new Vector2d(-55, 12), Math.toRadians(180))
+                                .lineToSplineHeading(new Pose2d(-45, 13.5, Math.toRadians(180)))
+                                .back(3)
+                                .lineToSplineHeading(new Pose2d(-20,13.5, Math.toRadians(270)))
                                 .waitSeconds(.5)
-                                .back(12)
-                                .lineToSplineHeading(new Pose2d(-20,12, Math.toRadians(270)))
-                                .waitSeconds(.5)
-                                .lineToSplineHeading(new Pose2d(-10,12, Math.toRadians(270)))
+                                .lineToSplineHeading(new Pose2d(-10,13.5, Math.toRadians(270)))
                                 .back(24)
                                 .build()
         );
