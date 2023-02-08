490462b5daccf26813b908b8aaeb918de8296078
SophieR25 <srocco@guilford.edu>
2023-02-08 17:31:25 -0500

    correct code*

--- MeepMeep/src/main/java/com/example/meepmeep/MeepMeepEngine2.java
+++ MeepMeep/src/main/java/com/example/meepmeep/MeepMeepEngine2.java
@@ -16,15 +16,11 @@ public class MeepMeepEngine2 {
         double poleDepth = 2;
 
         Pose2d rightStartPose = new Pose2d(-35, 61.5, Math.toRadians(270));
-<<<<<<< Updated upstream
-        Pose2d rightPrimaryPose = new Pose2d(-26, 7.25, Math.toRadians(-45));
-        Pose2d rightStackPose = new Pose2d(-48,12, Math.toRadians(180));
-=======
+
         Pose2d rightPrimaryPose = new Pose2d(-29, 5.75, Math.toRadians(-45));
         Vector2d rightMiddlePose2 = new Vector2d(-60, 12);
         Pose2d rightMiddlePose = new Pose2d(-36, 12, Math.toRadians(180));
         Pose2d rightStackPose = new Pose2d(-63,12, Math.toRadians(180));
->>>>>>> Stashed changes
 
         Pose2d leftStartPose = new Pose2d(35, 61.5, Math.toRadians(270));
         Pose2d leftPrimaryPose = new Pose2d(20, 12, Math.toRadians(270));
@@ -40,9 +36,6 @@ public class MeepMeepEngine2 {
                                 //slides go up during forward sequence
                                 .forward(34)
                                 .splineToSplineHeading(rightPrimaryPose, Math.toRadians(315))
-<<<<<<< Updated upstream
-                                .splineToSplineHeading(rightStackPose, Math.toRadians(315))
-=======
                                     //slides can go down about halfway through the linetosplineheading
                                 .lineToSplineHeading(rightMiddlePose)
                                 .lineToSplineHeading(rightStackPose)
@@ -51,24 +44,12 @@ public class MeepMeepEngine2 {
                                 .build()
 
                 );
-        RoadRunnerBotEntity rightCycle1 = new DefaultBotBuilder(meepMeep)
-                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
-                .setColorScheme(new ColorSchemeBlueLight())
-                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
-                .setDimensions(13.5, 17)
-                .followTrajectorySequence(drive ->
-                        drive.trajectorySequenceBuilder(rightStartPose)
-                                .splineTo(new Vector2d(-10, 24), Math.toRadians(0))
->>>>>>> Stashed changes
-                                .build()
 
-                );
 
         meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
                 .addEntity(rightCycle)
-                .addEntity(rightCycle1)
                 .start();
     }
 }
